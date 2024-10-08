383ecc22d95e06cb35b748f9151314efdc69f1c9
Victor Ramirez <vicdeveloper83@gmail.com>
2024-10-08 14:23:53 +0100

    prisma client created

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,28 @@
+import { prisma } from '@/data/postgres';
+import { Request, Response } from 'express';
+
+export const createUser = async (req: Request, res: Response) => {
+	try {
+		const { name, email, password, role, phone } = req.body;
+		if (!name || !email || !password || !role || !phone)
+			throw { message: ' Missing Information' };
+		const newUser = await prisma.user.create({
+			data: {
+				name,
+				email,
+				password,
+				role,
+				phone,
+			},
+		});
+		res.status(201).send({
+			status: 'success',
+			data: newUser,
+		});
+	} catch (error) {
+		res.status(400).send({
+			status: 'fail',
+			data: error,
+		});
+	}
+};
