682bd8c03637e2c6cf36f164a25182c486b93af8
Rocio Carle <rociocarle@yahoo.com.ar>
2024-10-09 22:15:21 -0300

    Mobile Banner Prototype

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PetCard from '../components/PetCard/PetCard';
 import NavBar  from '../components/Navbar/Navbar';
+import Banner  from '../components/Banner/Banner';
 
 function Home(){
     return (
         <>
             <NavBar />
+            <Banner  />
             <PetCard />
         </>
     );
