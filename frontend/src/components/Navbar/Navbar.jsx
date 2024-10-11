4bafccd53bd53e8b0735c73a9aa5afb388471610
alicia4079 <136130096+alicia4079@users.noreply.github.com>
2024-10-11 17:10:39 -0300

    navbar css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
-import React from "react";
 import Paw from '../../assets/paw-solid.svg'
 import BurgerMenu from '../../assets/bars-solid.svg'
 import './Navbar.css'
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
-    return (
-        <div className="nav-container">
-            <img className="logo" src={Paw} />
-            <nav>
-                <img className="burgerMenu" src={BurgerMenu} />
-            </nav>
-        </div>
-    );
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="nav-container">
+      <img className="logo" src={Paw} alt="Logo" />
+      <nav>
+        {isMobile ? (
+          <img className="burgerMenu" src={BurgerMenu} alt="Menú de hamburguesa" />
+        ) : (
+
+          <div className="desktop-menu">
+            <a href="#adopt" className="Adopt">Adoptar</a>
+            <a href="#contact" className="contact">Conócenos</a>
+            <a href="#login" className="login">Log in</a>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
 }
