8df36687d8cba4b86b649b8e66e134ebcc8b02f3
Claudio <ccmf.dev@gmail.com>
2024-10-16 11:11:38 -0300

    build: filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AllPets from './pages/AllPets';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline'; // Importa CssBaseline
+
 
 function App() {
   return (
     <>
-      <Home />
+      <CssBaseline />
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/AllPets" element={<AllPets />} />
+        </Routes>
+      </Router>
     </>
   )
 }
