03ccd341f3a90c2f12f445f48c0e51d95333f6c2
Claudio M. F. Castillo <97706777+ClaudioMFCDev@users.noreply.github.com>
2024-10-20 10:44:03 -0300

    Revert "Catalog"

--- frontend/src/components/FilterButton/FilterButton.jsx
+++ frontend/src/components/FilterButton/FilterButton.jsx
@@ -1,99 +1,28 @@
-import Btn from "./Btn";
-import "./FilterButton.css";
-import { useState } from "react";
+import { Button } from "@mui/material";
+import './FilterButton.css'
 
-// constante
-const filtros = {
-  gender: ["MALE", "FEMALE"], 
-  size: ["SMALL", "MEDIUM", "LARGE"],
-  age: ["PUPPY", "ADULT"]
-};
+const filtros = [
+    'macho', 'hembra', 'pequeño', 'mediano', 'grande'
+]
 
-///////// main  component
-const FilterButton = ({setActive, sendFilters}) => {
-  
-  // Manejamos los estados de los filtros
-  const [selectedFilters, setSelecterFilters] = useState({
-    gender: [],
-    size: [],
-    age: [],
-  });
-
-  // aply btn function
-  const handleClick = () => {
-    toggleActive();
-    handleApply();
-  }
-
-  const toggleActive = () => {
-    // cambia el estado activo/no activo en el padre
-    setActive((prevStatus) => !prevStatus)
-  }
-  
-
-  // Manejamos los click en los dif filtros
-  const handleFilterClick = (category, filtro) => {
-    setSelecterFilters((prevSelected) => {
-      const updatedCategory = prevSelected[category].includes(filtro)
-        ? prevSelected[category].filter((f) => f !== filtro)
-        : [...prevSelected[category], filtro];
-  
-      return {
-        ...prevSelected,
-        [category]: updatedCategory,
-      };
-    });
-  };
-
-  // Enviar los filtros a state
-  const handleApply = () => {
-    const selectedCategories = Object.entries(selectedFilters).reduce(
-      (acc, [category, filters]) => {
-        if (filters.length > 0) {
-          acc[category] = filters;
-        }
-        return acc;
-      },
-      {}
-    );
-
-    // Enviar los filtros seleccionados al padre
-    sendFilters(selectedFilters);
-    console.log("Filtros seleccionados: ", selectedCategories);
-    setSelecterFilters({
-      gender: [],
-      size: [],
-      age: [],
-    })
-  };
-
-  return (
-    <>
-      <div className="div-filters">
-        <div id="filters">
-          {
-            Object.entries(filtros).map(([key, values]) => (
-            <div id="div-category" key={key}>
-              <h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
-              <div id="span-container">
-                {values.map((value) => (
-                  <Btn
-                  key={value}
-                  text={value}
-                  onClick={() => handleFilterClick(key, value)}
-                  />
-                ))}
-              </div>
+const FilterButton = () => {
+    
+    return(
+        < >
+            <div className="div-filters">
+                <div id="filters">
+                    {filtros.map( (filtro) => (
+                        <Button key={filtro} sx={{ backgroundColor: 'transparent', height: 20, width: 80, fontSize:12, '&:hover': {
+                            backgroundColor: 'darkgray',
+                        }, }}  >{filtro}</Button>
+                    ) )}
+                </div>
+                <div id="aplicar-btn">
+                    <Button variant="outlined" size="small">Aplicar</Button>
+                </div>
             </div>
-          ))
-          }
-        </div>
-        <div id="aplicar-btn">
-          <button onClick={handleClick}>Aplicar</button>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    )
 }
 
 export default FilterButton;
--- frontend/src/pages/AllPets.jsx
+++ frontend/src/pages/AllPets.jsx
@@ -1,97 +1,25 @@
-import { useEffect, useState } from "react";
-import FilterButton from "../components/FilterButton/FilterButton";
-import Footer from "../components/Footer/Footer";
-import Card from "../components/PetCard/Card";
-import * as API from "../services/apiPetService";
+import FilterButton from '../components/FilterButton/FilterButton'
+import Footer from '../components/Footer/Footer';
+import Card from '../components/PetCard/Card'
 
-function randomArray(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
 
-///////main component
 const AllPets = () => {
-  const [pets, setPets] = useState([]);
-  const [isChildActive, setIsChildActive] = useState(false);
-  // Manejamos el estado de los filtros del child
-  const [selectedFilters, setSelectedFilters] = useState({
-    gender: [],
-    size: [],
-    age: [],
-  });
 
-  // Función que manejará los datos recibidos desde el hijo
-  const handleChildActiveStatus = (status) => {
-    setIsChildActive(status);
-  };
+    const pets = []
+    
 
-  // Función para manejar los filtros seleccionados desde el hijo
-  const handleFiltersFromChild = (filters) => {
-    setSelectedFilters(filters);
-  };
-
-  // Traemos las mascotas
-  useEffect(() => {
-    API.getAllPets()
-      .then((response) => {
-        // console.log("API response:", response);
-        if (response && Array.isArray(response.data)) {
-          const randomPets = randomArray(response.data);
-          setPets(randomPets);
-        }
-      })
-      .catch((error) => console.error("Error fetching pets:", error));
-  }, []);
-
-  // Filtrar las mascotas que cumplan con los filtros
-  const filteredPets = pets.filter((pet) => {
     return (
-      (selectedFilters.gender.length === 0 ||
-        selectedFilters.gender.includes(pet.gender)) &&
-      (selectedFilters.size.length === 0 ||
-        selectedFilters.size.includes(pet.size)) &&
-      (
-        selectedFilters.age.length === 0 ||
-        (selectedFilters.age.includes('PUPPY') && pet.age < 3) ||
-        (selectedFilters.age.includes('ADULT') && pet.age >= 3)
-      )
-    );
-  });
+        <>
+            <FilterButton/>
 
-  return (
-    <>
-      <FilterButton 
-      setActive={handleChildActiveStatus}
-      sendFilters={handleFiltersFromChild}
-      />
-      <div className="card-container">
-        { filteredPets.length > 0 ? (
-            filteredPets.map((pet) => (
-            <Card
-                key={pet.id}
-                name={pet.name}
-                age={pet.age}
-                gender={pet.gender}
-                size={pet.size}
-                image={pet.picture}
-            />
-        ))) : pets ? (
-          pets.map((pet) => (
-            <Card
-              key={pet.id}
-              name={pet.name}
-              age={pet.age}
-              gender={pet.gender}
-              size={pet.size}
-              image={pet.picture}
-            />
-          ))
-        ) : (
-          <p>No hay mascotas disponibles.</p>
-        )}
-      </div>
-      <Footer />
-    </>
-  );
-};
+            <div className='card-container'>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+            <Footer/>
+        </>
+    )
+}
 
 export default AllPets;
