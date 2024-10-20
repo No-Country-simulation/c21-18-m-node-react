9e63e6f5761dc45fd83ad7fc945eaa2bedb9cf22
Victor Ramirez <vicdeveloper83@gmail.com>
2024-10-20 18:52:14 +0100

    user routes create tested

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -8,15 +8,14 @@ import {
 } from '../controllers';
 const usersRouter = Router();
 
-usersRouter.route('/').post(createUser);
-
-usersRouter.post('/users', createUser);
+usersRouter.route('/create-user').post(createUser);
 
 usersRouter.get('/users', getAllUsers);
 
-usersRouter.get('/users/:id', getUserById);
+usersRouter
+	.route('/users/:id')
+	.get(getUserById)
+	.put(updateUser)
+	.delete(deleteUser);
 
-usersRouter.put('/users/:id', updateUser);
-
-usersRouter.delete('/users/:id', deleteUser);
 export default usersRouter;
