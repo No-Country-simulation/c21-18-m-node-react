afeb2321f7bdadbf7d9115c77dcf0b39fb971b06
Victor Ramirez <vicdeveloper83@gmail.com>
2024-10-21 10:34:45 +0100

    comment in roleCheck added

--- backend/src/middleware/roleCheck.ts
+++ backend/src/middleware/roleCheck.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-
+//when you call this middleware you have to indicate what roles are going to be allow to pass the check in an array e.g. roleCheck(['ADMIN','USER'])
 export const roleCheck = (allowedRoles: string[]) => {
 	return (req: Request, res: Response, next: NextFunction): void => {
 		if (!req.isAuthenticated()) {
