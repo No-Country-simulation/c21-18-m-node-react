d2d8b94d0267a58381168bc683829b026367dbc6
Rocio Carle <rociocarle@yahoo.com.ar>
2024-10-21 13:01:13 -0300

    datails

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,13 @@ export default {
                 './src/**/*.{js,ts,jsx,tsx}',],
   theme: {
     extend: {},
+    colors:{
+      anaranjado: '#ffac81',
+      rojo: '#ff928b',
+      piel: '#fec3a6',
+      amarillo: '#efe9ae',
+      verde: '#cdeac0',
+    }
   },
   plugins: [
     require('daisyui'),
