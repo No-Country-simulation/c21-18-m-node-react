2bc0c9e7ed6d299a2fee3ee8a2c5dc29d529e26d
Victor Ramirez <vicdeveloper83@gmail.com>
2024-10-22 19:43:19 +0100

    capture user cookies front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,12 +24,18 @@ const app = express();
 app.set('port', envs.PORT ?? 3001);
 
 // Middleware
-
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(morgan('dev'));
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 
 // Passport & session middleware
@@ -75,6 +81,6 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/pet', petRouter);
-app.use('/api/shelter', isAuthenticated, roleCheck(['ADMIN']), shelterRouter);
+app.use('/api/shelter', shelterRouter);
 
 export default app;
--- frontend/src/services/apiShelterService.js
+++ frontend/src/services/apiShelterService.js
@@ -1,29 +1,30 @@
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 export async function createShelter(
-  shelterName,
-  shelterAddress,
-  shelterPhoneNumber,
-  shelterEmail
+	shelterName,
+	shelterAddress,
+	shelterPhoneNumber,
+	shelterEmail
 ) {
-  const userToken = Cookies.get("connect.sid");
-  console.log(Cookies.get("connect.sid"));
-  console.log("userToken", userToken);
-  const response = await fetch(
-    "http://localhost:3000/api/shelter/create-shelter",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${userToken}`,
-      },
-      body: JSON.stringify({
-        shelterName,
-        shelterAddress,
-        shelterPhoneNumber,
-        shelterEmail,
-      }),
-    }
-  );
-  console.log(response);
-  return response.json();
+	const userToken = Cookies.get('connect.sid');
+	console.log(Cookies.get('connect.sid'));
+	console.log('userToken', userToken);
+	const response = await fetch(
+		'http://localhost:3000/api/shelter/create-shelter',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userToken}`,
+			},
+			body: JSON.stringify({
+				name: shelterName,
+				address: shelterAddress,
+				phone: shelterPhoneNumber,
+				email: shelterEmail,
+			}),
+			credentials: 'include', // Include credentials like cookies
+		}
+	);
+	console.log(response);
+	return response.json();
 }
