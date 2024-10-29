3c4a5e09e950d21d9807c88af053e69cd015bfc0
Rocio Carle <61993578+Rocksyro@users.noreply.github.com>
2024-10-29 10:16:39 -0300

    Update PetCard.jsx

--- frontend/src/components/PetCard/PetCard.jsx
+++ frontend/src/components/PetCard/PetCard.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './PetCard.css';
 
+import PropTypes from 'prop-types';
+
 export default function PetCard({ name, age, size, gender, image }) {
     return (
         <div className="card">
             <article className='pet-article'>
                 <img src={image} alt={name} />
                 <div className="pet-info">
-                    <span>{gender}</span>
-                    <span>{size ?? 'Unknown'}</span>
-                    <span>{age ?? 'Unknown'} años</span>
+                    <span>{gender ? gender.toLowerCase() : 'unknown'}</span>
+                    <span>{size ? size.toLowerCase() : 'unknown'}</span>
+                    <span>{age ? `${age.toString().toLowerCase()} años` : 'unknown años'}</span>
                 </div>
-                <p className='name'>{name}</p>
+                <p className='name'>{name ? name.toLowerCase() : 'unknown'}</p>
             </article>
         </div>
     );
 }
+
+PetCard.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    size: PropTypes.string,
+    gender: PropTypes.string,
+    image: PropTypes.string,
+};
