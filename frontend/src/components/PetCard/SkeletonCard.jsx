48d3e405f10a9afd460556f0d6bdb52b10103e85
Rocio Carle <rociocarle@yahoo.com.ar>
2024-10-30 11:51:10 -0300

    Skeleton funciona, falta darle estilos

--- frontend/src/components/PetCard/SkeletonCard.jsx
+++ frontend/src/components/PetCard/SkeletonCard.jsx
@@ -1,16 +1,14 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import './SkeletonCard.css'
+
 
 export const SkeletonCard = () => {
     return (
-        <SkeletonTheme baseColor='#a8a3a3' highlightColor='#444' duration={1}>
+        <SkeletonTheme baseColor='#d6d9d5' highlightColor='#bdbdbc' duration={1}>
             <div className='skeleton__group'>
                 <div className='skeleton__content'>
-                    <Skeleton className='skeleton__title' height={50} />
-                    <Skeleton height={20} count={2} />
+                    <Skeleton className='skeleton__title'/>
             </div>
-        <Skeleton className='skeleton__img' />
         </div>
     </SkeletonTheme>
     )
