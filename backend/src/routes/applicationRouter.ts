cc7606f11ec87bfa2d16ec0f1e6506964ce9338a
Victor Ramirez <vicdeveloper83@gmail.com>
2024-10-30 21:51:04 +0000

    application status

--- backend/src/routes/applicationRouter.ts
+++ backend/src/routes/applicationRouter.ts
@@ -1,8 +1,7 @@
-import express from 'express'
-import * as application from '../controllers/application'
-const applicationRouter = express.Router()
-
-
-applicationRouter.post('/create',application.createApplication)
-
-export default applicationRouter
+import express from 'express';
+import * as application from '../controllers/application';
+const applicationRouter = express.Router();
+applicationRouter.get('/', application.getAllApplications);
+applicationRouter.post('/create', application.createApplication);
+applicationRouter.put('/update/:id', application.updateApplicationStatus);
+export default applicationRouter;
