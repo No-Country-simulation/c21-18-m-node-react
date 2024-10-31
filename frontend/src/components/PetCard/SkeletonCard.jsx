4b06f6c8c2f3af9cb845a004a019bdc4b7ddd489
Rocio Carle <rociocarle@yahoo.com.ar>
2024-10-31 11:53:23 -0300

    Skeleton for pets in home working

--- frontend/src/components/PetCard/SkeletonCard.jsx
+++ frontend/src/components/PetCard/SkeletonCard.jsx
@@ -1,15 +1,27 @@
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export const SkeletonCard = () => {
     return (
-        <SkeletonTheme baseColor='#d6d9d5' highlightColor='#bdbdbc' duration={1}>
-            <div className='skeleton__group'>
-                <div className='skeleton__content'>
-                    <Skeleton className='skeleton__title'/>
+        <SkeletonTheme baseColor="#d6d9d5" highlightColor="#bdbdbc" duration={1}>
+            <div className="skeleton-card">
+                <article className="skeleton-article">
+                    {/* Skeleton para la imagen */}
+                    <Skeleton 
+                        width="80%" 
+                        height="200px" 
+                        style={{ borderRadius: '10px', margin: '30px 0' }} 
+                    />
+                    {/* Skeleton para el nombre */}
+                    <Skeleton 
+                        width="60%" 
+                        height="35px" 
+                        style={{ marginBottom: '15px', borderRadius: '4px' }} 
+                    />
+                </article>
             </div>
-        </div>
-    </SkeletonTheme>
-    )
-}
+        </SkeletonTheme>
+    );
+};
+
+
