cb30cb48c41d057b975ed05af87269b1568e577a
Rocio Carle <rociocarle@yahoo.com.ar>
2024-11-01 09:46:52 -0300

    ultimo cambio de skeleton

--- frontend/src/components/PetCard/SkeletonCard.jsx
+++ frontend/src/components/PetCard/SkeletonCard.jsx
@@ -1,4 +1,5 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import './SkeletonCard.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export const SkeletonCard = () => {
@@ -7,15 +8,16 @@ export const SkeletonCard = () => {
             <div className="skeleton-card">
                 <article className="skeleton-article">
                     {/* Skeleton para la imagen */}
-                    <Skeleton 
-                        width="80%" 
-                        height="200px" 
+                    <Skeleton className="skeleton-image" 
+                        height="200px"
+                        width="250px"
                         style={{ borderRadius: '10px', margin: '30px 0' }} 
                     />
                     {/* Skeleton para el nombre */}
                     <Skeleton 
-                        width="60%"
+                        className="skeleton-name"
                         height="35px"
+                        width="250px"
                         style={{ marginBottom: '15px', borderRadius: '4px' }} 
                     />
                 </article>
