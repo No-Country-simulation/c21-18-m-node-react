95e17002477d182cad42871667b2b5df29c4537f
javiercolodro <javiercolodro@gmail.com>
2024-11-06 13:57:53 -0300

    fix applicationTable styles

--- frontend/src/components/Tables/ApplicationTable.jsx
+++ frontend/src/components/Tables/ApplicationTable.jsx
@@ -13,6 +13,7 @@ import {
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import * as API from "../../services/apiAplicationService";
+import "./ApplicationTable.css";
 
 export const ApplicationTable = () => {
   const [applications, setApplications] = useState([]);
@@ -20,17 +21,15 @@ export const ApplicationTable = () => {
   useEffect(() => {
     const fetchApplications = async () => {
       try {
-        const { applications } = await API.getAllApplications();
+        const { applications } = await API.getAllApplications(); // Desestructurando aquí
         setApplications(applications);
         console.log("applications:", applications[0]);
       } catch (error) {
         console.error("Error fetching applications:", error);
       }
     };
-
     fetchApplications();
   }, []);
-
   const handleStatusChange = async (applicationId, newStatus, application) => {
     const {
       userId,
@@ -40,6 +39,7 @@ export const ApplicationTable = () => {
     const isApproved = newStatus === "APPROVED";
 
     try {
+      // Llamada al backend con los datos necesarios
       const updatedStatus = await API.toggleStatus(applicationId, newStatus, {
         userId,
         petId,
@@ -48,6 +48,7 @@ export const ApplicationTable = () => {
         isApproved,
       });
 
+      // Actualizar el estado en el frontend con el nuevo estado devuelto
       setApplications((prevApplications) =>
         prevApplications.map((app) =>
           app.id === applicationId ? { ...app, status: updatedStatus } : app
@@ -57,115 +58,99 @@ export const ApplicationTable = () => {
       console.error("Error en toggleStatus:", error);
     }
   };
-
-  const appStyle = {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "58vh",
-    padding: "20px",
-  };
-
-  const tableContainerStyle = {
-    flex: 1,
-    maxHeight: "60vh",
-  };
-
-
   return (
-    <div style={appStyle}>
-      <TableContainer style={tableContainerStyle}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Datos del Usuario</TableCell>
-              <TableCell align="center">Mascota</TableCell>
-              <TableCell align="center">Estado</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {applications.map((application) => (
-              <TableRow key={`${application.id}-${application.status}`}>
-                <TableCell>
-                  <Grid2 container spacing={2} alignItems="center">
-                    <Grid2 item xs={12} sm={4}>
-                      <img
-                        className="image"
-                        src={application.user.picture}
-                        alt="user image"
-                      />
-                    </Grid2>
-                    <Grid2 item xs={12} sm={8}>
-                      <div>
-                        <p>
-                          <strong>Nombre del usuario:</strong>
-                          {application.user.name}
-                        </p>
-                        <p>
-                          <strong>Solicitud para Mascota ID:</strong>
-                          {application.petId}
-                        </p>
-                        <p>
-                          <strong>Email del Solicitante:</strong>
-                          {application.user.email}
-                        </p>
-                        <p>
-                          <strong>¡Quiero adoptar a :</strong>
-                          {application.pet.name}!
-                        </p>
-                      </div>
-                    </Grid2>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Datos del Usuario</TableCell>
+            <TableCell align="center">Mascota</TableCell>
+            <TableCell align="center">Estado</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {applications.map((application) => (
+            <TableRow key={`${application.id}-${application.status}`}>
+              <TableCell>
+                <Grid2 container spacing={2} alignItems="center">
+                  <Grid2 item xs={12} sm={4}>
+                    <img
+                      className="image"
+                      src={application.user.picture}
+                      alt="user image"
+                    />
                   </Grid2>
-                </TableCell>
+                  <Grid2 item xs={12} sm={8}>
+                    <div>
+                      <p>
+                        <strong>Nombre del usuario:</strong>
+                        {application.user.name}
+                      </p>
+                      <p>
+                        <strong>Solicitud para Mascota ID:</strong>
+                        {application.petId}
+                      </p>
+                      <p>
+                        <strong>Email del Solicitante:</strong>
+                        {application.user.email}
+                      </p>
+                      <p>
+                        <strong>¡Quiero adoptar a :</strong>
+                        {application.pet.name}!
+                      </p>
+                    </div>
+                  </Grid2>
+                </Grid2>
+              </TableCell>
 
-                <TableCell>
-                  <Grid2
-                    container
-                    direction="column"
-                    spacing={1}
-                    alignItems="center"
+              <TableCell>
+                <Grid2
+                  container
+                  direction="column"
+                  spacing={1}
+                  alignItems="center"
+                >
+                  <Grid2 item xs={12} sm={4}>
+                    <img
+                      className="image"
+                      src={application.pet.picture}
+                      alt="pet image"
+                    />
+                  </Grid2>
+                  <Grid2 item xs={12}>
+                    <div>{application.pet.name}</div>
+                  </Grid2>
+                </Grid2>
+              </TableCell>
+
+              <TableCell>
+                <FormControl fullWidth>
+                  <InputLabel id={`status-select-label-${application.id}`}>
+                    Estado
+                  </InputLabel>
+                  <Select
+                    labelId={`status-select-label-${application.id}`}
+                    id={`status-select-${application.id}`}
+                    value={application.status}
+                    onChange={(event) =>
+                      handleStatusChange(
+                        application.id,
+                        event.target.value,
+                        application
+                      )
+                    }
+                    label="Estado"
                   >
-                    <Grid2 item xs={12} sm={4}>
-                      <img
-                        className="image"
-                        src={application.pet.picture}
-                        alt="pet image"
-                      />
-                    </Grid2>
-                    <Grid2 item xs={12}>
-                      <div>{application.pet.name}</div>
-                    </Grid2>
-                  </Grid2>
-                </TableCell>
-
-                <TableCell>
-                  <FormControl fullWidth>
-                    <InputLabel id={`status-select-label-${application.id}`}>
-                      Estado
-                    </InputLabel>
-                    <Select
-                      labelId={`status-select-label-${application.id}`}
-                      id={`status-select-${application.id}`}
-                      value={application.status}
-                      onChange={(event) =>
-                        handleStatusChange(
-                          application.id,
-                          event.target.value,
-                          application
-                        )
-                      }
-                      label="Estado"
-                    >
-                      <MenuItem value={"APPROVED"}>Aprobado</MenuItem>
-                      <MenuItem value={"DENIED"}>Denegado</MenuItem>
-                      <MenuItem value={"PENDING"}>Pendiente</MenuItem>
-                    </Select>
-                  </FormControl>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+                    <MenuItem value={"APPROVED"}>Aprobado</MenuItem>
+                    <MenuItem value={"DENIED"}>Denegado</MenuItem>
+                    <MenuItem value={"PENDING"}>Pendiente</MenuItem>
+                  </Select>
+                </FormControl>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
